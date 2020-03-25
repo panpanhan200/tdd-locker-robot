0cf4e48f8a4a34f8aeaa74c55be4cd86262eea69
Panpan Han <hanpanpan200@gmail.com>
2020-03-26 00:49:39 +0800

    Update locker capacity

--- src/main/java/cn/xpbootcamp/locker_robot/Locker.java
+++ src/main/java/cn/xpbootcamp/locker_robot/Locker.java
@@ -1,9 +1,35 @@
 package cn.xpbootcamp.locker_robot;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Locker {
-    public int capacity;
+    private int capacity;
+    List<Box> boxes = new ArrayList<>();
 
     public Locker(int capacity) {
         this.capacity = capacity;
+        for (int i = 0; i < capacity; i++) {
+            Box box = new Box();
+            boxes.add(box);
+        }
+    }
+
+    public void deposit() {
+        Optional<Box> targetBox = boxes.stream().filter(box -> !box.isOccupied).findFirst();
+        targetBox.ifPresent(box -> box.isOccupied = true);
+    }
+
+    public void setCapacity(int capacity) {
+        boolean isLockerInUse = boxes.stream().anyMatch(box -> box.isOccupied);
+        if (isLockerInUse) {
+            throw new RuntimeException("Set locker capacity failed");
+        }
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 }
--- src/test/java/cn/xpbootcamp/locker_robot/LockerTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/LockerTest.java
@@ -3,12 +3,34 @@ package cn.xpbootcamp.locker_robot;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class LockerTest {
     @Test
     void should_set_locker_capacity_successfully_when_init_locker_given_locker_is_not_initialized() {
         int capacity = 50;
         Locker locker = new Locker(capacity);
-        assertEquals(locker.capacity, capacity);
+        assertEquals(locker.getCapacity(), capacity);
+    }
+
+    @Test
+    void should_update_locker_capacity_successfully_when_update_locker_give_locker_is_initialized_and_not_in_use() {
+        int newCapacity = 50;
+        int oldCapacity = 15;
+        Locker locker = new Locker(oldCapacity);
+        locker.setCapacity(newCapacity);
+        assertEquals(locker.getCapacity(), newCapacity);
+    }
+
+    @Test
+    void should_return_set_locker_capacity_failed_message_when_update_locker_capacity_given_locker_is_in_use() {
+        int newCapacity = 50;
+        int oldCapacity = 15;
+        Locker locker = new Locker(oldCapacity);
+        locker.deposit();
+        Exception exception = assertThrows(RuntimeException.class, () ->
+                locker.setCapacity(newCapacity));
+        assertEquals("Set locker capacity failed", exception.getMessage());
     }
 }
