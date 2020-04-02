ebf3017c7c06d978d0ad1f57a54fe67c0b9d13ed
davidzwb <davidzwb@gmail.com>
2020-04-02 22:34:15 +0800

    Add test case for two lockers with one full

--- src/test/java/cn/xpbootcamp/locker_robot/PrimaryLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/PrimaryLockerRobotTest.java
@@ -27,5 +27,26 @@ public class PrimaryLockerRobotTest {
         assertTrue(lockerA.hasBag(bag));
     }
 
+    @Test
+    void should_deposit_bag_into_locker_a_and_return_ticket_when_robot_receives_one_bag_given_robot_has_two_lockers_named_a_and_b_with_order_a_b_with_capacity_one_and_a_full_b_not_full() {
+        List<Locker> lockerList = new ArrayList<Locker>();
+        Locker lockerA = new Locker(1);
+        Locker lockerB = new Locker(1);
+
+        lockerList.add(lockerA);
+        lockerList.add(lockerB);
+        Bag bagIgnored = new Bag();
+        lockerA.deposit(bagIgnored);
+
+        Robot robot = new Robot(lockerList);
+
+        Bag bag = new Bag();
+        Ticket ticket = robot.deposit(bag);
+
+        assertNotNull(ticket);
+        assertFalse(lockerA.hasBag(bag));
+        assertTrue(lockerB.hasBag(bag));
+    }
+
 
 }
