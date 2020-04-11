6c47d7f071e47766ba1bdbbbcd8317cb23050775
Panpan Han <hanpanpan200@gmail.com>
2020-04-11 15:40:15 +0800

    Update tasking and tests after review

--- src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartLockerRobotTest {
     @Test
-    void should_deposit_bag_to_B_and_return_ticket_when_robot_receives_one_bag_given_locker_A_with_1_empty_box_and_locker_B_with_2_empty_boxes() {
+    void should_deposit_bag_to_B_and_return_ticket_when_robot_deposits_one_bag_given_locker_A_with_1_empty_box_and_locker_B_with_2_empty_boxes() {
         Locker lockerA = new Locker(1);
         Locker lockerB = new Locker(2);
         List<Locker> lockers = new ArrayList<>();
@@ -23,9 +23,25 @@ public class SmartLockerRobotTest {
         assertNotNull(ticket);
         assertTrue(lockerB.hasBag(bag));
     }
-
+    
     @Test
-    void should_return_ticket_when_robot_receives_one_bag_given_locker_A_with_1_empty_box_and_locker_B_with_1_empty_box() {
+    void should_deposit_bag_to_A_and_return_ticket_when_robot_deposits_one_bag_given_locker_A_with_2_empty_boxes_and_locker_B_with_1_empty_box() {
+        Locker lockerA = new Locker(2);
+        Locker lockerB = new Locker(1);
+        List<Locker> lockers = new ArrayList<>();
+        lockers.add(lockerA);
+        lockers.add(lockerB);
+
+        SmartLockerRobot robot = new SmartLockerRobot(lockers);
+        Bag bag = new Bag();
+        Ticket ticket = robot.deposit(bag);
+
+        assertNotNull(ticket);
+        assertTrue(lockerA.hasBag(bag));
+    }
+    
+    @Test
+    void should_return_ticket_when_robot_deposits_one_bag_given_locker_A_with_1_empty_box_and_locker_B_with_1_empty_box() {
         Locker lockerA = new Locker(1);
         Locker lockerB = new Locker(1);
         List<Locker> lockers = new ArrayList<>();
@@ -37,10 +53,11 @@ public class SmartLockerRobotTest {
         Ticket ticket = robot.deposit(bag);
 
         assertNotNull(ticket);
+        assertTrue(lockerA.hasBag(bag));
     }
 
     @Test
-    void should_deposit_failed_when_robot_receives_one_bag_given_locker_A_with_0_empty_box_and_locker_B_with_0_empty_box() {
+    void should_deposit_failed_when_robot_deposits_one_bag_given_locker_A_with_0_empty_box_and_locker_B_with_0_empty_box() {
         Locker lockerA = new Locker(1);
         Locker lockerB = new Locker(1);
         List<Locker> lockers = new ArrayList<>();
@@ -70,6 +87,27 @@ public class SmartLockerRobotTest {
 
         Bag returnedBag = robot.withdraw(ticket);
         assertEquals(bag, returnedBag);
+    }
+
+    @Test
+    void should_return_bag1_when_withdraw_with_ticket_given_bag2_is_deposited_in_lockerB_and_ticket_is_returned_after_bag1_is_deposited_in_lockerA() {
+        List<Locker> lockerList = new ArrayList<>();
+        Locker lockerA = new Locker(1);
+        Locker lockerB = new Locker(1);
+        lockerList.add(lockerA);
+        lockerList.add(lockerB);
+        SmartLockerRobot robot = new SmartLockerRobot(lockerList);
+
+        Bag bag1 = new Bag();
+        Ticket ticket = robot.deposit(bag1);
+        Bag bag2 = new Bag();
+        robot.deposit(bag2);
+
+        assertTrue(lockerA.hasBag(bag1));
+        assertTrue(lockerB.hasBag(bag2));
+
+        Bag returnedBag = robot.withdraw(ticket);
+        assertEquals(bag1, returnedBag);
     }
 
     @Test
