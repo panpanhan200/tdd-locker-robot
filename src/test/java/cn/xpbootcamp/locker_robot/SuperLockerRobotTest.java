ec94717e3f283d28675bef0dce531ea167894bf7
davidzwb <davidzwb@gmail.com>
2020-04-16 21:14:11 +0800

    Add test case for vacancy rate in reverse order

--- src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
@@ -27,4 +27,25 @@ public class SuperLockerRobotTest {
         assertNotNull(ticket);
         assertTrue(lockerB.hasBag(bag));
     }
+
+    @Test
+    void should_deposit_bag_to_A_and_return_ticket_when_robot_deposits_one_bag_given_locker_A_with_vacancy_rate_100_percent_and_locker_B_with_vacancy_rate_50_percent() {
+        Locker lockerA = new Locker(2);
+        Locker lockerB = new Locker(2);
+
+        Bag bagB = new Bag();
+        lockerB.deposit(bagB);
+
+        List<Locker> lockers = new ArrayList<>();
+        lockers.add(lockerA);
+        lockers.add(lockerB);
+
+        SuperLockerRobot robot = new SuperLockerRobot(lockers);
+        Bag bag = new Bag();
+        Ticket ticket = robot.deposit(bag);
+
+        assertNotNull(ticket);
+        assertTrue(lockerA.hasBag(bag));
+    }
+
 }
